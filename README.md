e98014bcb12c0488011fbf75ecdc834eeb7a218c
hritiksingh1999 <hritik.singh@gslab.com>
2021-02-08 16:13:07 +0530

    uploading my first application on git

---
+++ README.md
@@ -0,0 +1,4 @@
+# CRUD APP USING FLASK AND POSTMAN
+
+1. In this application i have use python3,flask,postman
+2. The operations are create,read,update,delete.
---
+++ myflask1.py
@@ -0,0 +1,32 @@
+from flask import Flask,jsonify,request,redirect
+
+
+app=Flask(__name__)
+
+songs=[{'name':'e','writer':'s'},{'name': 'o',"writer":"e"}]
+'''@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url=request.url.replace('http://','https://')
+        return redirect(url), 200
+'''
+@app.route("/songs",methods=["GET","POST"])
+def get_songs():
+    if request.method=="GET":
+        return (songs)
+    else:
+        song=request.get_json()
+        songs.append(song)
+        return {'index': len(songs)},200
+
+@app.route("/songs/<int:index>",methods=["PUT","DELETE"])
+def put_songs(index):
+    if request.method=="PUT":
+        song=request.get_json()
+        songs[index]=song
+        return (songs),200
+    else:
+        songs.pop(index)
+        return "DELETED",200
+
+app.run(ssl_context=('cert.pem', 'key.pem'))
